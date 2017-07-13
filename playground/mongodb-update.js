47fd4c6b8341f6f64aabf0dc2761ef26b9e04572
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-12 20:29:11 -0600

    merge branch  to master

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,20 +8,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db)=>{
     console.log('Connected to MongoDB Server');
 
     //findOneAndUpdate
-    // db.collection('Todos').findOneAndUpdate({
-    //     _id: new ObjectID('5966cf67ee28fa4286ef2490')
-    // }, 
-    // {
-    //     $set:{
-    //         completed: true
-    //     }
-    // },
-    // {
-    //     returnOriginal: false
-    // })
-    // .then((result)=>{
-    //     console.log(result);
-    // });
+    db.collection('Todos').findOneAndUpdate({
+        _id: new ObjectID('5966cf67ee28fa4286ef2490')
+    }, 
+    {
+        $set:{
+            completed: true
+        }
+    },
+    {
+        returnOriginal: false
+    })
+    .then((result)=>{
+        console.log(result);
+    });
 
     db.collection('Users').findOneAndUpdate({
         _id: new ObjectID('59643731cda02cf688dd1045')
