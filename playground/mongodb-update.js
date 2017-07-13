c307adc50de66d7564045f2c9b6881567cbb2c44
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-12 20:27:42 -0600

    complete course 68. Update data by using .findOneAndUpdate(). Try to use  to update string and  to increase number

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db)=>{
+    if(err){
+        console.log('unable to connect to mongoDB server');
+    }
+    console.log('Connected to MongoDB Server');
+
+    //findOneAndUpdate
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5966cf67ee28fa4286ef2490')
+    // }, 
+    // {
+    //     $set:{
+    //         completed: true
+    //     }
+    // },
+    // {
+    //     returnOriginal: false
+    // })
+    // .then((result)=>{
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('59643731cda02cf688dd1045')
+    }, {
+        $set:{
+            name: 'Update'
+        },
+        $inc:{
+            age: 1
+        }
+    },{
+        returnOriginal: false
+    }).then((result)=>{
+        console.log(result);
+    })
+});
