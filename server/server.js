497225202cedaa3ffdb1394a6a961b5b5fd93280
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-14 20:27:59 -0600

    complete course 73. User POST

--- server/server.js
+++ server/server.js
@@ -10,7 +10,15 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res)=>{
-    console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text,
+    });
+    todo.save().then((doc)=>{
+        res.send(doc);
+    }, (e)=>{
+        //这个是http status， 400代表 bad request
+        res.status(400).send(e);
+    })
 });
 
 app.listen(3000, ()=>{
