21205598727861876127d69a1125bbb3ea733df9
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-23 13:56:24 -0600

    finish course 78 GET/todo/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,31 @@ app.get('/todos', (req, res)=>{
     }, (e)=>{
         res.status(400).send(e);
     });
+});
+
+//GET
+app.get('/todos/:id',(req, res)=>{
+    /* request params is going to be an object that it's going to have key value 
+    pairs where the key is URL parameter like ID and the value is whatever value 
+    was actually put there*/
+    var id = req.params.id;
+
+    //.isValid()=>Checks if a value is a valid bson ObjectID
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    };
+    
+    //.catch()=>Attaches a callback for only the rejection of the Promise.
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            res.status(404).send();
+        };
+
+        //{todo} is equal to {todo: todo}
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
 });
 
 app.listen(3000, ()=>{
