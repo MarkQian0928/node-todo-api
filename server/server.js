64d04245dbb3c9a7cc04807ac3816b714174880b
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-23 19:45:13 -0600

    finish course 81. PATCH/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -74,6 +75,32 @@ app.delete('/todos/:id',(req, res)=>{
         res.status(400).send();
     });
 });
+
+//PATCH
+app.patch('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    };
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    // new: true 和 mongodb-update 里的returnOriginal: false作用一样，都表示return new object
+    Todo.findByIdAndUpdate(id, {$set: body},{new: true}).then((todo)=>{
+        if(!todo){
+            return res.send(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+});
+
 
 app.listen(port, ()=>{
     console.log(`Started on port ${port}`);
