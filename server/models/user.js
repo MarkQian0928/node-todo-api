16336971ffec5a36c2092cb72f36d7f513ef7e4a
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-29 20:44:42 -0600

    complete course 88, set up the user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,36 @@
 
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
+//unique: true 代表了property email does not have the same value as anyother documents in the collection
+//validate: --> 去mongoose custom validate查， 要用到third party library, validate npm
 var User = mongoose.model('User',{
     email:{
         type: String,
-        require: true,
+        required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message:'{VALUE} is not a valid email'
+        }
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength:6
+    },
+    tokens:[{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type:String,
+            required: true
+        }
+    }]
 });
 
 module.exports = {User};
